c0932eb8efa5416d6a681b5a1fa0445f955bd1f1
Usman Shaukat Gill <usman.gill@tuni.fi>
2024-09-17 02:25:08 +0300

    Completed Guess Game using DOM Manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+const checkButton = document.querySelector('.check');
+checkButton.addEventListener('click', () => {
+    const guessedNumber = Number(document.querySelector('.guess').value)
+
+    if (!guessedNumber) {
+        document.querySelector('.message').textContent = 'No number!'
+    }
+
+    if (guessedNumber === randomNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!'
+    } else if (guessedNumber > randomNumber) {
+        document.querySelector('.message').textContent = 'Too high!'
+    } else if (guessedNumber < randomNumber) {
+        document.querySelector('.message').textContent = 'Too low!'
+    }
+})
